48e37abb21e6cb6e6ae7b1c47dc00211d205859b
blisn8405 <blisn8405@wrdsb.ca>
2020-09-15 15:16:10 +0000

    question 3

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+   // create a scanner for user input
+Scanner input = new Scanner(System.in);
+
+   //define a variable for the integer 3
+   final int A =  3;
+
+   //define a variable for the integer 5
+   final int B = 5;
+
+  //put the ascending numbers in order
+  if(A<= B){
+   System.out.println("Your numbers in ascending order are 3, 5");
+  } else {
+  System.out.println("Your numbers in ascending order are 5, 3");
+  }
+ 
+ //define a variable for the integer 4
+ final int C = 4;
+
+ //define a variable for the integer 4
+ final int D = 4;
+ if(C<= D){
+ System.out.println("Your numbers in ascending order are 4, 4");
+ }
   }
 }
